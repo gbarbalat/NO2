5d2b4e50743ae12a665a1253ccd4c7737b1593fd
gbarbalat <103890190+gbarbalat@users.noreply.github.com>
2026-05-06 17:13:27 +0200

    Update ALL_SCRIPTS_MPI.sh

--- 1km/ALL_SCRIPTS_MPI.sh
+++ 1km/ALL_SCRIPTS_MPI.sh
@@ -92,15 +92,33 @@ oarsub -S ./preproc_NDVI_MPI.sh | tee out_preproc_NDVI_MPI.tmp
 eval `egrep -o "OAR_JOB_ID=.*" out_preproc_NDVI_MPI.tmp`
 jobid_preproc_NDVI_MPI=$OAR_JOB_ID
 
-oarsub -S ./preproc_CAMS_MPI_be42005.sh | tee out_preproc_CAMS_MPI_be42005.tmp
-eval `egrep -o "OAR_JOB_ID=.*" out_preproc_CAMS_MPI_be42005.tmp`
-jobid_preproc_CAMS_MPI=$OAR_JOB_ID_be42005
+oarsub -S ./preproc_CAMS_MPI.sh | tee out_preproc_CAMS_MPI.tmp
+eval `egrep -o "OAR_JOB_ID=.*" out_preproc_CAMS_MPI.tmp`
+jobid_preproc_CAMS_MPI=$OAR_JOB_ID
 
 oarsub -a $jobid_preproc_IGN1_MPI -a $jobid_preproc_IGN2_MPI -a $jobid_preproc_IGN3_MPI -a $jobid_preproc_IGN4_MPI -a $jobid_preproc_IGN5_MPI -a $jobid_preproc_IGN6_MPI -a $jobid_preproc_IGN7_MPI -a $jobid_preproc_IGN8_MPI -a $jobid_preproc_ERA5_asn_MPI -a $jobid_preproc_ERA5_blh_00_MPI -a $jobid_preproc_ERA5_blh_12_MPI -a $jobid_preproc_ERA5_d2m_MPI -a $jobid_preproc_ERA5_e_MPI -a $jobid_preproc_ERA5_sp_MPI -a $jobid_preproc_ERA5_ssr_MPI -a $jobid_preproc_ERA5_t2m_mean_MPI -a $jobid_preproc_ERA5_t2m_sd_MPI -a $jobid_preproc_ERA5_tcc_MPI -a $jobid_preproc_ERA5_tp_MPI -a $jobid_preproc_ERA5_u10_MPI -a $jobid_preproc_ERA5_v10_MPI -S ./try_parallel_preproc_MPI.sh | tee out_try_parallel_preproc_MPI.tmp
 eval `egrep -o "OAR_JOB_ID=.*" out_try_parallel_preproc_MPI.tmp`
 jobid_try_parallel_preproc_MPI=$OAR_JOB_ID
 
-oarsub -a $jobid_try_parallel_preproc_MPI -S ./prepare_df_Model2_MPI.sh | tee out_prepare_df_Model2_MPI.tmp
+
+oarsub -a $jobid_try_parallel_preproc_MPI -S ./prepare_df_Model1_MPI.sh -a $OAR_JOB_ID | tee out_prepare_df_Model1_MPI.tmp
+eval `egrep -o "OAR_JOB_ID=.*" out_prepare_df_Model1_MPI.tmp`
+jobid_prepare_df_Model1_MPI=$OAR_JOB_ID
+
+oarsub -a $jobid_prepare_df_Model1_MPI -S ./Model1_ranger_not_caret_MPI_PARTI.sh -a $OAR_JOB_ID | tee out_Model1_ranger_not_caret_MPI_PARTI.tmp
+eval `egrep -o "OAR_JOB_ID=.*" out_Model1_ranger_not_caret_MPI_PARTI.tmp`
+jobid_Model1_ranger_not_caret_MPI_PARTI=$OAR_JOB_ID
+
+oarsub -a $jobid_prepare_df_Model1_MPI -S ./Model1_ranger_not_caret_MPI_PARTII.sh -a $OAR_JOB_ID | tee out_Model1_ranger_not_caret_MPI_PARTII.tmp
+eval `egrep -o "OAR_JOB_ID=.*" out_Model1_ranger_not_caret_MPI_PARTII.tmp`
+jobid_Model1_ranger_not_caret_MPI_PARTII=$OAR_JOB_ID
+
+oarsub -a $jobid_prepare_df_Model1_MPI -S ./Model1_ranger_not_caret_MPI_PARTIII.sh -a $OAR_JOB_ID | tee out_Model1_ranger_not_caret_MPI_PARTIII.tmp
+eval `egrep -o "OAR_JOB_ID=.*" out_Model1_ranger_not_caret_MPI_PARTIII.tmp`
+jobid_Model1_ranger_not_caret_MPI_PARTIII=$OAR_JOB_ID
+
+
+oarsub -a $jobid_Model1_ranger_not_caret_MPI_PARTII -a $jobid_Model1_ranger_not_caret_MPI_PARTII -a $jobid_Model1_ranger_not_caret_MPI_PARTIII -S ./prepare_df_Model2_MPI.sh | tee out_prepare_df_Model2_MPI.tmp
 eval `egrep -o "OAR_JOB_ID=.*" out_prepare_df_Model2_MPI.tmp`
 jobid_prepare_df_Model2_MPI=$OAR_JOB_ID
 
