3b4b5f0fef96fc5e0a689cca7ece4e116c249c53
gbarbalat <103890190+gbarbalat@users.noreply.github.com>
2026-05-09 18:00:06 +0200

    adjust time to process on gricad

--- 1km/sh scripts/preproc_ERA5_d2m_MPI.sh
+++ 1km/sh scripts/preproc_ERA5_d2m_MPI.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #OAR -n preproc_ERA5_d2m_MPI
-#OAR -l /nodes=1,walltime=1:00:00
+#OAR -l /nodes=1,walltime=5:00:00
 #OAR --stdout preproc_ERA5_d2m_MPI.out
 #OAR --stderr preproc_ERA5_d2m_MPI.err
 #OAR --project edenpelagie
