25b0cf0684f4043afb0fa8d42218403da240ba89
gbarbalat <103890190+gbarbalat@users.noreply.github.com>
2026-05-25 14:49:50 +0200

    Update prepare_df_Model2_MPI.R

--- 1km/R scripts/prepare_df_Model2_MPI.R
+++ 1km/R scripts/prepare_df_Model2_MPI.R
@@ -28,6 +28,19 @@ monitor_coord=no2.sf %>% mutate(time=datey) %>% select(-datey)
 file_is=paste0("data4_Model2_",current_year,".RData")#is a raster of omi prediction
 load(file=paste0(path_to,file_is)) 
 st_crs(data4_Model2)=2154
+
+# 1. Re-create your date sequence for the grid
+dates_sequence <- seq(as.Date(paste0(current_year, "-01-01")), by = "1 day", length.out = days_in_total)
+
+# 2. Fix the stars grid dimension
+data4_Model2 <- data4_Model2 %>%
+  st_set_dimensions(which = "time", values = dates_sequence)
+
+# 3. FIX THE MONITOR COORDINATES:
+# Convert the integer time (1, 2, 3...) in your points to the matching Date object
+monitor_coord <- monitor_coord %>%
+  mutate(time = dates_sequence[as.numeric(time)])
+
 df_final_Model2 = data4_Model2 %>% 
 		st_extract(at=monitor_coord, time_column="time") %>% 
  		st_join(monitor_coord) %>%
@@ -39,10 +52,14 @@ df_final_Model2 =df_final_Model2 %>% cbind(coords) %>% st_drop_geometry()
 (head(df_final_Model2))
 #save(df_final_Model2,file=paste0("df_final_Model2_var",2,"_",current_year,".RData"))
 
-for (i in 3:40) {#take out OMI and CAMS
+for (i in 2:40) {#DO NOT take out OMI and CAMS, so start from 1
 file_is=paste0("updated_DMraster_France_var",i,"_",current_year,".RData")#is a raster 
 load(file=paste0(path_to,file_is)) 
 #st_crs(data4_Model2)=2154
+# Fix the stars grid dimension
+updated_DMraster <- updated_DMraster %>%
+  st_set_dimensions(which = "time", values = dates_sequence)
+
 
 tmp = updated_DMraster %>% 
 		st_extract(at=monitor_coord, time_column="time") %>% 
@@ -53,6 +70,8 @@ tmp = updated_DMraster %>%
 		st_drop_geometry() 
 
 df_final_Model2= df_final_Model2 %>% cbind(tmp)
+
+print(i)
 }
 
 (head(df_final_Model2))
