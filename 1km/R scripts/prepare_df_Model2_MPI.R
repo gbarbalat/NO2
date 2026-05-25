884b19379a8c9a6587039c3158d4a825f735f82f
gbarbalat <103890190+gbarbalat@users.noreply.github.com>
2026-05-25 21:59:37 +0200

    Update prepare_df_Model2_MPI.R

--- 1km/R scripts/prepare_df_Model2_MPI.R
+++ 1km/R scripts/prepare_df_Model2_MPI.R
@@ -52,7 +52,7 @@ df_final_Model2 =df_final_Model2 %>% cbind(coords) %>% st_drop_geometry()
 (head(df_final_Model2))
 #save(df_final_Model2,file=paste0("df_final_Model2_var",2,"_",current_year,".RData"))
 
-for (i in 2:40) {#DO NOT take out OMI and CAMS, so start from 1
+for (i in 2:40) {#DO NOT take out OMI as you are taking "predictions", so start from 1
 file_is=paste0("updated_DMraster_France_var",i,"_",current_year,".RData")#is a raster 
 load(file=paste0(path_to,file_is)) 
 #st_crs(data4_Model2)=2154
